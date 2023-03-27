723f0d014c56c8ef6947da5f918ad3cb928a8223
AyatoTomofuji <114973958+AyatoTomofuji@users.noreply.github.com>
2023-03-28 05:58:19 +0900

    Update Controller.py

--- src/Controller.py
+++ src/Controller.py
@@ -6,35 +6,45 @@ from func import angle360
 from kesslergame import Ship
 
 
+# 距離5分割
 class Controller(KesslerController):
     """
          A ship controller class for Kessler. This can be inherited to create custom controllers that can be passed to the
         game to operate within scenarios. A valid controller contains an actions method that takes in a ship object and ass
         game_state dictionary. This action method then sets the thrust, turn_rate, and fire commands on the ship object.
         """
-
     def __init__(self):
-        gene2 = [-516.7926396, -386.32765888, 65.06318108, -399.52026909, -124.68190627,
-                  158.16590569, 94.60270925, 109.68473244, -25.23185839, 76.2294561,
-                  143.31836882, 123.88324814, 127.25818193, 126.01031619, 124.56793154,
-                  119.17246516, 129.83427421, 117.047472]
-        gene = [-30.46403303, 29.93242511, 80.11163356, 88.23208249]
+        gene = [-164.53175401560333, -53.11381500132402, 99.55962719106978, 652.0695605148196, 958.2536948627446, 113.17313354331807]
+        genes2 = [-422.98248909, -128.46239109, 395.65025775, -339.31340805, -82.99984531,
+                157.18145777, 94.03193966, 74.20410544, -141.6155565, 66.7441948,
+                105.5832539, 136.26770441, 440.24368511, -32.15986455, -269.37155599,
+                -3.07185922, 180.88739761, -17.52924744, -11.0651477, 105.48644365,
+                25.6119877, 56.20575568, 85.31037087, 156.41788735, 13.28000091,
+                75.04230663, 145.83883738, -5.34633099, 79.93202705, 170.01952603, ]
         """
         Create your fuzzy logic controllers and other objects here
         """
-        left = gene[0]
-        center = gene[1]
-        right = gene[2]
-        center_angle = gene[3]
+        left1 = gene[0] 
+        left2 = gene[1] 
+        center = gene[2]
+        right2 = gene[3]
+        right1 = gene[4]
+        center_angle = gene[5]
 
         def membership1(x):
-            if x <= gene[1]:
-                return np.array([1.0 - (x - left) / (center - left), (x - left) / (center - left), 0.0])
-            elif x <= right:
-                return np.array([0.0, right / (right - center) - x / (right - center),
-                                 x / (right - center) - center / (right - center)])
+            if x <= left1:
+                return [1.0, 0.0, 0.0, 0.0, 0.0]
+            elif x <= left2:
+                return np.array([1.0 - (x - left1) / (left2 - left1), (x - left1) / (left2 - left1), 0.0, 0.0, 0.0])
+            elif x <= center:
+                return np.array([0.0, (center - x) / (center - left2),
+                                 (x - left2) / (center - left2), 0.0, 0.0])
+            elif x <= right2:
+                return np.array([0.0, 0.0, (right2 - x) / (right2 - center), (x - center) / (right2 - center), 0.0])
+            elif x <= right1:
+                return np.array([0.0, 0.0, 0.0, (right1 - x) / (right1 - right2), (x - right2) / (right1 - right2)])
             else:
-                return np.array([0.0, 0.0, 1.0])
+                return np.array([0.0, 0.0, 0.0, 0.0, 1.0])
 
         def membership2(angle):
             angle = abs(angle)
@@ -45,33 +55,29 @@ class Controller(KesslerController):
 
         self.membership1 = membership1
         self.membership2 = membership2
-        self.center = gene[1]
 
         def mems(x, angle):
-            Rules = {
-                0: "x1 is near  and x2 is front ",
-                1: "x1 is near  and x2 is middle",
-                2: "x1 is near  and x2 is back",
-                3: "x1 is close and x2 is front",
-                4: "x1 is close and x2 is middle",
-                5: "x1 is close and x2 is back",
-                6: "x1 is far   and x2 is front",
-                7: "x1 is far   and x2 is middle",
-                8: "x1 is far   and x2 is back",
-            }
+            nonlocal genes2
+            genes_out = np.array(genes2).copy()
 
-            out0 = np.array([gene2[0], gene2[9]])
-            out1 = np.array([gene2[1], gene2[10]])
-            out2 = np.array([gene2[2], gene2[11]])
-            out3 = np.array([gene2[3], gene2[12]])
-            out4 = np.array([gene2[4], gene2[13]])
-            out5 = np.array([gene2[5], gene2[14]])
-            out6 = np.array([gene2[6], gene2[15]])
-            out7 = np.array([gene2[7], gene2[16]])
-            out8 = np.array([gene2[8], gene2[17]])
+            out0 = np.array([genes_out[0], genes_out[15]])
+            out1 = np.array([genes_out[1], genes_out[16]])
+            out2 = np.array([genes_out[2], genes_out[17]])
+            out3 = np.array([genes_out[3], genes_out[18]])
+            out4 = np.array([genes_out[4], genes_out[19]])
+            out5 = np.array([genes_out[5], genes_out[20]])
+            out6 = np.array([genes_out[6], genes_out[21]])
+            out7 = np.array([genes_out[7], genes_out[22]])
+            out8 = np.array([genes_out[8], genes_out[23]])
+            out9 = np.array([genes_out[9], genes_out[24]])
+            out10 = np.array([genes_out[10], genes_out[25]])
+            out11 = np.array([genes_out[11], genes_out[26]])
+            out12 = np.array([genes_out[12], genes_out[27]])
+            out13 = np.array([genes_out[13], genes_out[28]])
+            out14 = np.array([genes_out[14], genes_out[29]])
+
             k = membership1(x)
             p = membership2(angle)
-
             rule0 = k[0] * p[0]
             rule1 = k[0] * p[1]
             rule2 = k[0] * p[2]
@@ -81,27 +87,36 @@ class Controller(KesslerController):
             rule6 = k[2] * p[0]
             rule7 = k[2] * p[1]
             rule8 = k[2] * p[2]
+            rule9 = k[3] * p[0]
+            rule10 = k[3] * p[1]
+            rule11 = k[3] * p[2]
+            rule12 = k[4] * p[0]
+            rule13 = k[4] * p[1]
+            rule14 = k[4] * p[2]
             out = ((rule0 * out0) + (rule1 * out1) + (rule2 * out2) + (rule3 * out3) + (rule4 * out4) + (
                     rule5 * out5) + (rule6 * out6) + (
-                           rule7 * out7) + (rule8 * out8))
+                           rule7 * out7) + (rule8 * out8) + (rule9 * out9) + (rule10 * out10) + (rule11 * out11) + (
+                           rule12 * out12) + (rule13 * out13) + (rule14 * out14))
             return out
 
         self.mems = mems
+
+        center_x = 500
+        center_y = 400
 
     def actions(self, ownship: Dict, input_data: Dict[str, Tuple]) -> Tuple[float, float, bool]:
         # timeout(input_data)
         # 隕石と機体の位置関係のセクション
         ast_list = np.array(input_data["asteroids"])
+        # (x,y)で表す，機体からの距離
         dist_xylist = [np.array(ownship['position']) - np.array(ast['position']) for ast in ast_list]
-        dist_avoidlist = dist_xylist.copy()
+        dist_avoid_list = dist_xylist.copy()
         dist_list1 = [math.sqrt(xy[0] ** 2 + xy[1] ** 2) for xy in dist_xylist]
-        closest = np.argmin(dist_list1)
-        dist_closest1 = dist_list1[closest]
 
         # よける部分に関しては画面端のことを考える，弾丸はすり抜けないから狙撃に関しては考えない
         sidefromcenter = 500 - ownship['position'][0]
         below_center = 400 - ownship['position'][1]
-        for xy in dist_avoidlist:
+        for xy in dist_avoid_list:
             if xy[0] > 500:
                 xy[0] -= 1000
             elif xy[0] < -500:
@@ -110,17 +125,18 @@ class Controller(KesslerController):
                 xy[1] -= 800
             elif xy[1] < -400:
                 xy[1] += 800
-        dist_avoidlist = [math.sqrt(xy[0] ** 2 + xy[1] ** 2) for xy in dist_avoidlist]
+        dist_avoid_list = [math.sqrt(xy[0] ** 2 + xy[1] ** 2) for xy in dist_avoid_list]
 
-        sorted2_idx = np.argsort(dist_avoidlist)
+        sorted2_idx = np.argsort(dist_avoid_list)
         sorteddict = ast_list[sorted2_idx]
+        # ここから考えるのは近傍5つの隕石
         search_list = sorteddict[0:5]
         search_dist = np.array([math.dist(ownship['position'], ast['position']) for ast in search_list])
-        near_angle = [np.array(ast['position']) - np.array(ownship['position']) for ast in search_list]
-        near_angle = [angle360(math.degrees((np.arctan2(near_ang[1], near_ang[0])))) - ownship['heading'] for near_ang
-                      in near_angle]
+        angle_dist = [np.array(ast['position']) - np.array(ownship['position']) for ast in search_list]
+        angle_dist = [angle360(math.degrees((np.arctan2(near_ang[1], near_ang[0])))) - ownship['heading'] for near_ang
+                      in angle_dist]
         aalist = []
-        for ang in near_angle:
+        for ang in angle_dist:
             if ang > 180:
                 ang -= 360
             elif ang < -180:
@@ -128,13 +144,23 @@ class Controller(KesslerController):
             aalist.append(ang)
 
         angdiff_front = min(aalist, key=abs)
-        angdiff = aalist[np.argmin(search_dist)]
+        angdiff = aalist[0]
         fire_bullet = abs(angdiff_front) < 10 and min(dist_list1) < 400
-        avoidance = np.min(dist_avoidlist)
-        """if len(input_data['ships']) >= 2:
-            dist = math.dist(ownship['position'], input_data['ships'][1-ownship['id']]['position'])
-            if dist < 20:
-                avoidance = dist"""
+        avoidance = np.min(dist_avoid_list)
+
+        if len(input_data['ships']) >= 2:
+            angle_ships = ast_angle(ownship['position'], input_data['ships'][2 - ownship['id']]['position'])
+            dist = math.dist(ownship['position'], input_data['ships'][2 - ownship['id']]['position'])
+            if (dist <= avoidance):
+                if angle_ships > 180:
+                    angle_ships -= 360
+                elif angle_ships < -180:
+                    angle_ships += 360
+                avoidance = dist
+                angdiff = abs(angle_ships)
+                print(dist)
+                print(angle_ships)
+
         rule = self.mems(avoidance, angdiff)
 
         thrust = rule[0]
@@ -148,8 +174,9 @@ class Controller(KesslerController):
             turn_rate = ownship['turn_rate_range'][1]
         elif turn_rate < ownship['turn_rate_range'][0]:
             turn_rate = ownship['turn_rate_range'][0]
+        # 前後，回転，射撃のタプルをリターンする
         return thrust, turn_rate, fire_bullet
 
     @property
     def name(self) -> str:
-        return "OMU-let's"
+        return "OMU-Let's"
